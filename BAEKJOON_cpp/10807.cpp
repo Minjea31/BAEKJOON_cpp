ceb3cb2765abaf070b472e4285ad48b716eb61d8
kimmi <kimminjea31@naver.com>
2024-05-01 23:37:38 +0900

    5/1 백준

---
+++ BAEKJOON_cpp/10807.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int set = 0;
+
+	cin >> set;
+
+	int number[100];
+
+	for (int i = 0; i < set; i++)
+	{
+		cin >> number[i];
+	}
+
+	int f_num = 0;
+
+	cin >> f_num;
+
+	int count = 0;
+
+	for (int i = 0; i < set; i++)
+	{
+		if (f_num == number[i])
+		{
+			count += 1;
+		}
+	}
+
+	cout << count;
+}
---
+++ BAEKJOON_cpp/25304.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int sum = 0;
+	int num = 0;
+	int price = 0;
+	int num_p = 0;
+	int result = 0;
+
+	cin >> sum;
+
+	cin >> num;
+
+	for (int i = 0; i < num; i++)
+	{
+		cin >> price >> num_p;
+		result += price * num_p;
+	}
+
+	if (sum == result)
+		cout << "Yes";
+	else
+		cout << "No";
+}
