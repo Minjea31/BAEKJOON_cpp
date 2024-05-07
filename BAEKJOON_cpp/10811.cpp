6e5f2b9431e902dd68b81b9b89c7a077e59a52af
kimmi <kimminjea31@naver.com>
2024-05-08 00:55:59 +0900

    5/8

---
+++ BAEKJOON_cpp/10811.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int set = 0;
+	int size = 0;
+	int num[100] = { 0 };
+
+	int num_f = 0;
+	int num_l = 0;
+
+	cin >> size >> set;
+
+	for (int i = 1; i <= size; i++)
+	{
+		num[i] = i;
+	}
+
+	while (set--)
+	{
+		cin >> num_f >> num_l;
+
+		for (int i = 0; i <= (num_l - num_f) / 2; i++)
+		{
+			swap(num[num_f + i], num[num_l - i]);
+		}
+	}
+
+	for (int i = 1; i <= size; i++)
+	{
+		cout << num[i] <<' ';
+	}
+}
