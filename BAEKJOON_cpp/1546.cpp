b83ff9c2782d1a1d77651a18a401981d8bf6f746
kimmi <kimminjea31@naver.com>
2024-05-08 18:14:31 +0900

    5/8

---
+++ BAEKJOON_cpp/1546.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int set = 0;
+	cin >> set;
+
+	float score[1000] = { 0 };
+
+	for (int i = 1; i <= set; i++)
+	{
+		cin >> score[i];
+	}
+
+	int MAX = 0;
+	for (int i = 1; i <= set; i++)
+	{
+		if (MAX < score[i])
+		{
+			MAX = score[i];
+		}
+	}
+
+
+	double sum = 0;
+	for (int i = 1; i <= set; i++)
+	{
+		score[i] = (score[i] /MAX) * 100;
+		sum += score[i];
+	}
+
+	double avrg = 0;
+	avrg = sum / set;
+	cout << avrg;
+}
