d89c1a4bb5a3fbcb9eb529591fcd2fd127a9aa98
kimmi <kimminjea31@naver.com>
2024-05-14 13:51:12 +0900

    5/14

---
+++ BAEKJOON_cpp/10809.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string sen;
+	cin >> sen;
+
+	for (int i = 97; i < 123; i++)
+	{
+		char voca;
+		voca = i;
+		if (sen.find(voca, 0) <= (sen.length() - 1))
+		{
+			cout << sen.find(voca, 0) << " ";
+		}
+		else
+			cout << "-1" << " ";
+	}
+}
---
+++ BAEKJOON_cpp/2675.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int num = 0;
+	cin >> num;
+	int* cycle = new int[num];
+	string* sen = new string[num];
+
+	for (int i = 0; i < num; i++)
+	{
+		cin >> cycle[i] >> sen[i];
+	}
+
+	for (int i = 0; i < num; i++)
+	{
+		for (int x = 0; x < sen[i].length(); x++)
+		{
+			for (int j = 0; j < cycle[i]; j++)
+			{
+				cout << sen[i].substr(x, 1);
+			}
+		}
+		cout << endl;
+	}
+}
