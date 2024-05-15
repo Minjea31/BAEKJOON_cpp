e95f308f16c1dcde3fce5e31a1acb84b2e59649f
kimmi <kimminjea31@naver.com>
2024-05-16 01:30:58 +0900

    5/15

---
+++ BAEKJOON_cpp/1152.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string sen;
+	getline(cin, sen);
+
+	int count = 1;
+
+	if (sen.length() == 1 && sen[0] == ' ') {
+		cout << 0;
+		return 0;
+	}
+
+	for (int i = 1; i < sen.length() - 1; i++) {
+		if (sen[i] == ' ')
+			count++;
+	}
+
+	cout << count;
+}
