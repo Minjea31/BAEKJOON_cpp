2a60bd50e80034fbc106ce5ca51c3423219a02b1
kimmi <kimminjea31@naver.com>
2024-05-21 15:47:15 +0900

    5/21

---
+++ BAEKJOON_cpp/5622.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string sen;
+	cin >> sen;
+
+	int time = 0;
+
+
+	for (int i = 0; i < sen.length(); i++)
+	{
+		char a = sen.substr()
+		switch()
+	}
+}
