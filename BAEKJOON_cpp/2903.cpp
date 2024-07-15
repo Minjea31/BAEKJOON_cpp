26f629f9ae240bb3851f4f6f2f611150d2b80faa
kimmi <kimminjea31@naver.com>
2024-07-16 00:07:00 +0900

    24.07.15

---
+++ BAEKJOON_cpp/2903.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int count;
+    cin >> count;
+    int length = pow(2, count) + 1;
+    int sum = pow(length, 2);
+
+    cout << sum;
+}
