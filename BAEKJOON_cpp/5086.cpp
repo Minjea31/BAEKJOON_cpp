a54cf8f6ea3ba8a6d1d10f0dc1b6c506ca230d19
kimmi <kimminjea31@naver.com>
2024-08-10 14:47:37 +0900

    24.08.10

---
+++ BAEKJOON_cpp/5086.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num_1, num_2;
+	cin >> num_1 >> num_2;
+
+	while (num_1 != 0 && num_2 != 0)
+	{
+		if (num_2 % num_1 == 0)
+		{
+			cout << "factor" << "\n";
+		}
+		else if (num_1 % num_2 == 0)
+		{
+			cout << "multiple" << "\n";
+		}
+		else
+		{
+			cout << "neither" << "\n";
+		}
+		cin >> num_1 >> num_2;
+	}
+}
