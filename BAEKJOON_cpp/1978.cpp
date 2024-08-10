fb9ffeaf2f4bd771bef314935552fa85bb99aa58
kimmi <kimminjea31@naver.com>
2024-08-11 03:07:30 +0900

    24.08.11

--- BAEKJOON_cpp/1978.cpp
+++ BAEKJOON_cpp/1978.cpp
@@ -3,5 +3,26 @@ using namespace std;
 
 int main()
 {
+	int n;
+	cin >> n;
+	int count = 0;
 
+	while (n--)
+	{
+		int num;
+		cin >> num;
+		if (num != 1 && num != 2)
+		{
+			for (int i = 2; i < num; i++)
+			{
+				if (num % i == 0)
+					break;
+				else if (i == num - 1)
+					count += 1;
+			}
+		}
+		else if (num == 2)
+			count += 1;
+	}
+	cout << count;
 }
---
+++ BAEKJOON_cpp/2581.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int M, N;
+	int sum = 0, min = -1;
+	int count = 0;
+	cin >> M >> N;
+
+	for (int i = M; i <= N; i++) 
+	{
+		for (int j = 1; j <= i; j++) 
+		{
+			if (i % j == 0)
+				count++;
+		}
+		if (count == 2) 
+		{		
+			if (min == -1)		
+				min = i;
+			sum += i;
+		}
+		count = 0;
+	}
+
+	if (min == -1)
+		cout << -1 << '\n';
+	else
+		cout << sum << '\n' << min << '\n';
+}
