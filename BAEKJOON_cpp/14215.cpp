0b5cba62d79906c7cca1f7c070f333c1278fc2a4
kimmi <kimminjea31@naver.com>
2024-08-13 11:10:52 +0900

    24.08.13

---
+++ BAEKJOON_cpp/14215.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int s[3];
+	cin >> s[0] >> s[1] >> s[2];
+
+	int MAX = s[0];
+	for (int i = 1; i < 3; i++)
+	{
+		if (MAX < s[i])
+		{
+			MAX = s[i];
+		}
+	}
+
+	if (s[0] + s[1] + s[2] - MAX > MAX)
+	{
+		cout << s[0] + s[1] + s[2];
+	}
+	else if (s[0] + s[1] + s[2] - MAX <= MAX)
+	{
+		cout << (s[0] + s[1] + s[2] -MAX) * 2 -1;
+	}
+}
---
+++ BAEKJOON_cpp/5073.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	while (true)
+	{
+		int s[3];
+		cin >> s[0] >> s[1] >> s[2];
+
+		if (s[0] == 0 && s[1] == 0 && s[2] == 0)
+			break;
+
+		int MAX = s[0];
+		int num = 0;
+		for (int i = 1; i < 3; i++)
+		{
+			if (MAX < s[i])
+			{
+				MAX = s[i];
+				
+			}
+		}
+		
+		if (MAX >= s[0] + s[1] || MAX >= s[1] + s[2] || MAX >= s[0] + s[2])
+		{
+			cout << "Invalid" << endl;
+		}
+		else
+		{
+			if (s[0] == s[1] && s[1] == s[2])
+			{
+				cout << "Equilateral" << endl;
+			}
+			else if (s[0] == s[1] || s[1] == s[2] || s[0] == s[2])
+			{
+				cout << "Isosceles" << endl;
+			}
+			else
+				cout << "Scalene" << endl;
+		}
+	}
+}
