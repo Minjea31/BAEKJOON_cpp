8dea2a117539ad184483b5523c226e0e81447cc1
kimmi <kimminjea31@naver.com>
2024-08-25 02:46:28 +0900

    24.08.25

---
+++ BAEKJOON_cpp/2798.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x, sum;
+	cin >> x >> sum;
+	int MAX = 0;
+
+	int* num = new int[x];
+	for (int i = 0; i < x; i++)
+	{
+		cin >> num[i];
+	}
+
+	for (int i = 0; i < x - 2; i++)
+	{
+		for (int j = i + 1; j < x - 1; j++)
+		{
+			for (int k = j + 1; k < x; k++)
+			{
+				if (num[i] + num[j] + num[k] <= sum)
+				{
+					if (sum - (num[i] + num[j] + num[k]) <= sum - MAX)
+					{
+						MAX = num[i] + num[j] + num[k];
+					}
+				}
+			}
+		}
+	}
+
+	cout << MAX;
+	delete[] num;
+}
